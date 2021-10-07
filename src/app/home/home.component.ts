7cb436aebf2de75c24d4b82bf7f075f7fdc63a52
Ishant <er.ishant@gmail.com>
2021-10-07 13:19:10 +0530

    Query Params

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  query: string = "No Data";
+  name: string = "No Data";
+
+  // We do not create the Objects of Components
+  // hence, Angular FW is creating Objects on behalf of us
+  // Inversion of Control : IOC
+
+  // That means, we must not use the constructorto initialize or pass some data as input defined by developer
+  // As of now we have added ActivatedRoute which will be automatically initialized by Angular
+
+  // Dependency Injection
+  constructor(private route: ActivatedRoute) { 
+
+  }
 
   ngOnInit(): void {
+
+    this.route.queryParams.subscribe(params => {
+      this.query = params['query']
+      this.name = params['name']
+    });
+
+
   }
+
 
 }
