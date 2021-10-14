1523f99440481f1eab0dfb2ff7b8a05555d96cef
Ishant <er.ishant@gmail.com>
2021-10-14 13:01:54 +0530

    Authntication Module and Add User Data in DB

--- src/app/authenticate/authenticate.component.ts
+++ src/app/authenticate/authenticate.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from 'firebase/auth';
+import { getFirestore, collection, addDoc } from '@firebase/firestore/lite'
+import { DBService } from '../db.service';
 
 @Component({
   selector: 'app-authenticate',
@@ -9,6 +11,8 @@ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sig
 })
 export class AuthenticateComponent implements OnInit {
 
+  uid: string = "NA";
+
   authForm = new FormGroup(
     {
       email: new FormControl(''),
@@ -16,22 +20,53 @@ export class AuthenticateComponent implements OnInit {
     }
   );
 
-  constructor() { }
+  constructor(private db: DBService) { }
 
   ngOnInit(): void {
   }
 
   registerUser(){
     //console.log(this.authForm.value);
-    /*const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, this.authForm.value.email, this.authForm.value.password)
     .then((userCredential) => {
         console.log("User Created in Auth Module");
-        
+        const user = userCredential.user;
+        this.uid = user.uid;
+
+        // Save the user in DataBase :)
+        const firestoreDB = getFirestore(this.db.app);
+        const usersCollection = collection(firestoreDB, 'users');
+        addDoc(usersCollection, {
+          name: '',
+          phone: '',
+          email: this.authForm.value.email,
+          profileImage: '',
+          address: '',
+          uid: this.uid 
+        });
     })
     .catch((error) =>{
       console.log("Something Went Wrong");
-    });*/
+    });
   }
+
+  signInUser(){
+    const auth = getAuth();
+
+    //signOut(auth);
+
+    signInWithEmailAndPassword(auth, this.authForm.value.email, this.authForm.value.password)
+    .then((userCredential) => {
+        console.log("User Signed in Auth Module");
+        const user = userCredential.user;
+        this.uid = user.uid;
+    })
+    .catch((error) =>{
+      console.log("Something Went Wrong");
+    });
+  }
+
+
 
 }
