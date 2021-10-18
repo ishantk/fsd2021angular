9521e56e57526be911c8145c99586cc59a80c63a
Ishant <er.ishant@gmail.com>
2021-10-18 15:14:19 +0530

    Restaurant Form Creation

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Restaurant } from '../model/restaurant';
 import { RestaurantsService } from '../restaurants.service';
 
@@ -15,6 +16,28 @@ export class RestaurantsComponent implements OnInit {
     new Restaurant("Subway", 15, 4.8, "sandwich, salads")
   ];*/
 
+  cities = [
+    {cityName: "Select City", state:""},
+    {cityName: "Ludhina", state:"Punjab", pinCode: [141001, 141002, 141005]},
+    {cityName: "Chadigarh", state:"Punjab"},
+    {cityName: "Amritsar", state:"Punjab"},
+    {cityName: "Jalandhar", state:"Punjab"},
+    {cityName: "Phagwara", state:"Punjab"},
+  ];
+
+  restaurantForm = new FormGroup(
+    {
+      name: new FormControl(''),
+      phone: new FormControl(''),
+      email: new FormControl(''),
+      veg: new FormControl(false),
+      nonVeg: new FormControl(false),
+      servingType: new FormControl('delivery'),
+      city: new FormControl(this.cities[0]),
+      image: new FormControl('')
+    }
+  );
+
   restaurants = this.service.getRestaurants();
 
   // if the above array is suppose to be created by fetching data from Server
@@ -28,5 +51,10 @@ export class RestaurantsComponent implements OnInit {
 
   addRestaurant(name: string, timeToDeliver: string, ratings: string, categories: string){
     this.restaurants.push(new Restaurant(name, Number(timeToDeliver), Number(ratings), categories))
+  }
+
+  addRestaurantToFirebase(){
+    console.log(this.restaurantForm.value);
+    
   }
 }
